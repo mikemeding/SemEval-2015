e3ece565c965bc9fa2c3c6e14605ab92f27ecf4c
mikemeding <mikeymeding@gmail.com>
2015-03-15 13:56:41 -0400

    Hoanh, contact me for the decrypyting password for data if you need it

--- scripts/pit2015_eval_single.py
+++ scripts/pit2015_eval_single.py
@@ -1,5 +1,5 @@
 # ==============  SemEval-2015 Task 1  ==============
-#  Paraphrase and Semantic Similarity in Twitter
+# Paraphrase and Semantic Similarity in Twitter
 # ===================================================
 #
 # Author: Wei Xu (UPenn xwe@cis.upenn.edu)
@@ -26,10 +26,10 @@ from os.path import isfile, join
 from pit2015_checkformat import CheckFileFormat
 
 
-
 def average(x):
     assert len(x) > 0
     return float(sum(x)) / len(x)
+
 
 def pearson(x, y):
     assert len(x) == len(y)
@@ -46,146 +46,148 @@ def pearson(x, y):
         diffprod += xdiff * ydiff
         xdiff2 += xdiff * xdiff
         ydiff2 += ydiff * ydiff
-        
+
     if xdiff2 == 0 or ydiff2 == 0:
-    	return 0.0
+        return 0.0
 
     return diffprod / math.sqrt(xdiff2 * ydiff2)
 
+
 def EvalSingleSystem(testlabelfile, outputfile):
+    # read in golden labels
+    goldlabels = []
+    goldscores = []
 
-	# read in golden labels
-	goldlabels = []
-	goldscores = []
-	
-	hasscore = False
-	with open(testlabelfile) as tf:
-		for tline in tf:
-			tline = tline.strip()
-			tcols = tline.split('\t')
-			if len(tcols) == 2:
-				goldscores.append(float(tcols[1]))
-				if tcols[0] == "true":
-					goldlabels.append(True)
-				elif tcols[0] == "false":
-					goldlabels.append(False)
-				else:
-					goldlabels.append(None)
+    hasscore = False
+    with open(testlabelfile) as tf:
+        for tline in tf:
+            tline = tline.strip()
+            tcols = tline.split('\t')
+            if len(tcols) == 2:
+                goldscores.append(float(tcols[1]))
+                if tcols[0] == "true":
+                    goldlabels.append(True)
+                elif tcols[0] == "false":
+                    goldlabels.append(False)
+                else:
+                    goldlabels.append(None)
 
-	
-	
-	
-	# read in system labels
-	syslabels = []
-	sysscores = []
-	
-	with open(outputfile) as of:
-		for oline in of:
-			oline = oline.strip()
-			ocols = oline.split('\t')
-			if len(ocols) == 2:
-				sysscores.append(float(ocols[1]))
-				if float(ocols[1]) > 0.001 :
-					hasscore = True
-				if ocols[0] == "true":
-					syslabels.append(True)
-				elif ocols[0] == "false":
-					syslabels.append(False)
 
-	# evaluation metrics
-	
-	# system binary labels vs golden binary labels
-	# F1 / Precision / Recall
-	sysoutputs = []
-	tp = 0
-	fp = 0
-	tn = 0
-	fn = 0
-	for (i,syslabel) in enumerate(syslabels):
-	
-		if syslabel == True and goldlabels[i] == True:
-			tp += 1
-		elif syslabel == True and goldlabels[i] == False:
-			fp += 1
-		elif syslabel == False and goldlabels[i] == False:
-			tn += 1
-		elif syslabel == False and goldlabels[i] == True:
-			fn += 1
-		
-	P = tp / (tp + fp)
-	R = tp / (tp + fn)
-	F = 2 * P * R / (P + R)
-	
-	testsize = str(tp + fn + fp + tn)
 
-	bar = "\t"
-	none = "---"
-	matches = re.match(r'.*PIT2015_([^_]*)_(.*).output', outputfile)
-	teamname = matches.groups(1)[0]
-	nameofrun = matches.groups(1)[1]
 
-	if hasscore == False:
-		evalresult = testsize + bar + teamname + bar + nameofrun + bar + bar + "{0:.3f}".format(F) + bar + "{0:.3f}".format(P) + bar + "{0:.3f}".format(R) \
-			+ bar + bar + none + bar + none + bar + none + bar + none
-		return evalresult
+    # read in system labels
+    syslabels = []
+    sysscores = []
 
-	# system degreed scores vs golden binary labels
-	# maxF1 / Precision / Recall  
-    
-	maxF1 = 0
-	P_maxF1 = 0
-	R_maxF1 = 0
-    
+    with open(outputfile) as of:
+        for oline in of:
+            oline = oline.strip()
+            ocols = oline.split('\t')
+            if len(ocols) == 2:
+                sysscores.append(float(ocols[1]))
+                if float(ocols[1]) > 0.001:
+                    hasscore = True
+                if ocols[0] == "true":
+                    syslabels.append(True)
+                elif ocols[0] == "false":
+                    syslabels.append(False)
+
+    # evaluation metrics
+
+    # system binary labels vs golden binary labels
+    # F1 / Precision / Recall
+    sysoutputs = []
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    for (i, syslabel) in enumerate(syslabels):
+
+        if syslabel == True and goldlabels[i] == True:
+            tp += 1
+        elif syslabel == True and goldlabels[i] == False:
+            fp += 1
+        elif syslabel == False and goldlabels[i] == False:
+            tn += 1
+        elif syslabel == False and goldlabels[i] == True:
+            fn += 1
+
+    P = tp / (tp + fp)
+    R = tp / (tp + fn)
+    F = 2 * P * R / (P + R)
+
+    testsize = str(tp + fn + fp + tn)
+
+    bar = "\t"
+    none = "---"
+    matches = re.match(r'.*PIT2015_([^_]*)_(.*).output', outputfile)
+    teamname = matches.groups(1)[0]
+    nameofrun = matches.groups(1)[1]
+
+    if hasscore == False:
+        evalresult = testsize + bar + teamname + bar + nameofrun + bar + bar + "{0:.3f}".format(
+            F) + bar + "{0:.3f}".format(P) + bar + "{0:.3f}".format(R) \
+                     + bar + bar + none + bar + none + bar + none + bar + none
+        return evalresult
+
+    # system degreed scores vs golden binary labels
+    # maxF1 / Precision / Recall
+
+    maxF1 = 0
+    P_maxF1 = 0
+    R_maxF1 = 0
+
     # rank system outputs according to the probabilities predicted
-	sortedindex = sorted(range(len(sysscores)), key = sysscores.__getitem__)   
-	sortedindex.reverse() 
+    sortedindex = sorted(range(len(sysscores)), key=sysscores.__getitem__)
+    sortedindex.reverse()
 
-	truepos  = 0
-	falsepos = 0
-	
-	for sortedi in sortedindex:
-		if goldlabels[sortedi] == True:
-			truepos += 1
-		elif goldlabels[sortedi] == False:
-			falsepos += 1
-            
-		precision = 0
-		
-		if truepos + falsepos > 0:
-			precision = truepos / (truepos + falsepos)
-		
-		recall = truepos / (tp + fn)
-		f1 = 0
+    truepos = 0
+    falsepos = 0
 
-		#print precision, recall
-		    
-		if precision + recall > 0:
-			f1 = 2 * precision * recall / (precision + recall)
-			if f1 > maxF1:
-				maxF1 = f1
-				P_maxF1 = precision
-				R_maxF1 = recall
+    for sortedi in sortedindex:
+        if goldlabels[sortedi] == True:
+            truepos += 1
+        elif goldlabels[sortedi] == False:
+            falsepos += 1
 
-	# system degreed scores  vs golden degreed scores 
-	# Pearson correlation
-	
-	
-	pcorrelation = pearson(sysscores, goldscores)
-		
-	evalresult = testsize + bar + teamname + bar + nameofrun + bar + bar + "{0:.3f}".format(F) + bar + "{0:.3f}".format(P) + bar + "{0:.3f}".format(R) \
-		+ bar + bar + "{0:.3f}".format(pcorrelation) + bar + "{0:.3f}".format(maxF1) + bar + "{0:.3f}".format(P_maxF1) + bar + "{0:.3f}".format(R_maxF1) 
-		
-		
-	return evalresult
-	
+        precision = 0
+
+        if truepos + falsepos > 0:
+            precision = truepos / (truepos + falsepos)
+
+        recall = truepos / (tp + fn)
+        f1 = 0
+
+        #print precision, recall
+
+        if precision + recall > 0:
+            f1 = 2 * precision * recall / (precision + recall)
+            if f1 > maxF1:
+                maxF1 = f1
+                P_maxF1 = precision
+                R_maxF1 = recall
+
+    # system degreed scores  vs golden degreed scores
+    # Pearson correlation
+
+
+    pcorrelation = pearson(sysscores, goldscores)
+
+    evalresult = testsize + bar + teamname + bar + nameofrun + bar + bar + "{0:.3f}".format(F) + bar + "{0:.3f}".format(
+        P) + bar + "{0:.3f}".format(R) \
+                 + bar + bar + "{0:.3f}".format(pcorrelation) + bar + "{0:.3f}".format(maxF1) + bar + "{0:.3f}".format(
+        P_maxF1) + bar + "{0:.3f}".format(R_maxF1)
+
+    return evalresult
+
 
 def PITEval(labelfile, outfile):
-	if CheckFileFormat(labelfile, outfile):
-		return EvalSingleSystem(labelfile, outfile)
-	else:
-		print "System output file format error: " + outputfile
+    if CheckFileFormat(labelfile, outfile):
+        return EvalSingleSystem(labelfile, outfile)
+    else:
+        print "System output file format error: " + outputfile
 
-	return
+    return
 
 # Evaluate all system outputs 
 #
@@ -201,8 +203,7 @@ def PITEval(labelfile, outfile):
 
 # Evaluate a single system output
 if __name__ == "__main__":
+    testlabelfile = sys.argv[1]
+    outputfile = sys.argv[2]
 
-	testlabelfile = sys.argv[1]
-	outputfile    = sys.argv[2]
-	
-	print PITEval(testlabelfile, outputfile)
+    print PITEval(testlabelfile, outputfile)
